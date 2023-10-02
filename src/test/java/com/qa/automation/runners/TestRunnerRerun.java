d2f2857b31de646d147f2ee322cb24744b83ebe4
matemayur <matemayur786@gmail.com>
2023-10-02 15:45:20 +0530

    configuration for failed test ReRun

--- src/test/java/com/qa/automation/runners/TestRunnerRerun.java
+++ src/test/java/com/qa/automation/runners/TestRunnerRerun.java
@@ -1,33 +1,27 @@
 package com.qa.automation.runners;
 
 import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features="classpath:features",//to tell cucumber where is ur feature file
+        features="@target/failedrerun.txt",//to tell cucumber where is ur feature file
         glue="com.qa.automation.stepdefs", // to tell cucumber where is ur step def code
         tags="", // to tell which tagged feature file to execute
         plugin = {"pretty", // to generate reports
-            "html:target/html/htmlreport.html",
-            "json:target/json/file.json",
-            "junit:target/cucumberXML.xml",
-            "rerun:target/failedrerun.txt",
-            "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-			"timeline:test-output-thread/"
+            "html:target/html/html_rerun_report.html",
+            "json:target/json/file_rerun_report.json",
+            "junit:target/cucumberXML_rerun_report.xml",
             },
         monochrome = true,
         publish=true,
         dryRun=false // to tell whether to test run(true) or actual run(false)
         )
 
-public class TestRunner {
-	
-	//Class will be Empty.
-    //Nothing goes here
-    //So do not get confused
-	
-	
+
+
+public class TestRunnerRerun {
 
 }
